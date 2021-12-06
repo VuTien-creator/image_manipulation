28b2765f0bd307b13a98acbf75398bb336a53ae3
VuTien-creator <tienlv304@gmail.com>
2021-12-06 11:37:45 +0700

    Task: Image Manipulation

--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ImageManipulation extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','path','type','data', 'output_path', 'user_id', 'album_id'];
 }
