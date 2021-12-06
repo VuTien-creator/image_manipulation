8bb8d323ea76cc0fe656b92b4c7d41d5b465f8f7
VuTien-creator <tienlv304@gmail.com>
2021-12-06 11:36:39 +0700

    Task: Complete Album Task

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\V1\AlbumController;
+use App\Http\Controllers\V1\ImageManipulationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function (){
+
+    Route::apiResource('album',AlbumController::class);
+    Route::apiResource('image',ImageManipulationController::class);
+    
 });
